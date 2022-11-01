2cb682314ec53551eb6a2a8a5cf7967dbdafb316
Shoaib Zeshan <szeeshan707@gmail.com>
2022-11-01 20:11:15 +0530

    adding more functionalites

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ const displayMovments = function (movements) {
     const html = `
           <div class="movements__row">
           <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">${mov}€</div>
         </div>`
 
     containerMovements.insertAdjacentHTML('afterbegin', html)
@@ -78,9 +78,24 @@ const displayMovments = function (movements) {
 }
 displayMovments(account1.movements)
 
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`
+
+  const out = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`
+
+
+  const interest = movements.filter(mov => mov > 0).map(deposit => (deposit * 1.2) / 100).filter((int, i, arr) => { return int >= 1 })
+    .reduce((acc, int) => acc + int, 0)
+  labelSumInterest.textContent = `${interest}€`
+}
+calcDisplaySummary(account1.movements)
+
 const calcDisplayBalance = (movements) => {
   const balance = movements.reduce((acc, mov) => acc + mov, 0)
-  labelBalance.textContent = `${balance} EUR`
+  labelBalance.textContent = `${balance}€`
 }
 calcDisplayBalance(account1.movements)
 
@@ -109,3 +124,4 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+console.log(movements)
